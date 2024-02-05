6247452b4e8ad0bbcab5e938d16db64f59fa9918
Job Busienei <106816642+JT-tanui@users.noreply.github.com>
2024-02-05 22:17:01 +0300

    The first ruby commit
    These are basically a sketch of the Ruby language.

---
+++ greetings.rb
@@ -0,0 +1,15 @@
+module Tools
+  def sayhi(name)
+    puts "Hello #{name}"    
+  end
+  def saybye(name)
+    puts"Bye #{name}"
+  end
+end
+
+# include Tools
+# Tools.sayhi("Tanui")
+# Tools.saybye("Tanui")
+
+# The commented code is used to
+# to use the method/ function in the module 
